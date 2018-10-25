cfde30369561b6fa8db912f2572e1b974e4d487b
MACBOOK <zkunitsa@yahoo.com>
2018-10-25 13:46:31 -0700

    note entry form

--- MidtermJavaFx/src/view/NotesAppUI.java
+++ MidtermJavaFx/src/view/NotesAppUI.java
@@ -2,13 +2,16 @@ package view;
 
 import controller.Controller;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -24,18 +27,17 @@ public class NotesAppUI extends Application {
         stage.show();
     }
 
-    private Scene getScene()
-    {
+    private Scene getScene() {
         VBox mainPanel = new VBox();
         mainPanel.setPadding(new Insets(10));
         mainPanel.setSpacing(10);
 
-        mainPanel.getChildren().addAll(dataInputScreen(),dataViewScreen());
+        mainPanel.getChildren().addAll(dataInputScreen(), dataViewScreen());
 
         return new Scene(mainPanel, 1300, 600);
     }
 
-    private HBox dataInputScreen(){
+    private HBox dataInputScreen() {
         HBox mainPanel = new HBox();
         VBox vPanel = new VBox();
         Label label = new Label("Notes");
@@ -59,10 +61,51 @@ public class NotesAppUI extends Application {
         return mainPanel;
     }
 
-    private HBox dataViewScreen(){
+    private HBox dataViewScreen() {
         HBox panel = new HBox();
+        panel.setAlignment(Pos.TOP_RIGHT);
+        VBox Vpanel = new VBox();
+        Vpanel.setSpacing(10);
 
+        TextField title = new TextField();
+        TextArea note = new TextArea();
+        ComboBox<String> list = new ComboBox<>();
+
+        list.getItems().addAll("Quote", "URL", "Code", "TO-DO");
+
+        /*list.valueProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                type = newValue;
+            }
+        });*/
+
+        Button save = new Button("Save");
+
+        Vpanel.getChildren().addAll(
+                list,
+                createTextInput("Title:  ", title),
+                createTextInput("Note: ", note),
+                save);
+
+        save.setOnAction(event -> {
+            controller.handleNewNote(list.getValue(), title.getText(), note.getText());
+        });
+
+        panel.getChildren().addAll(Vpanel);
 
         return panel;
     }
+
+    private HBox createTextInput(String prompt, Node inputElement)
+    {
+        Label label = new Label(prompt);
+        HBox panel = new HBox();
+        panel.getChildren().addAll(label, inputElement);
+
+        return panel;
+    }
+
+
 }
+
